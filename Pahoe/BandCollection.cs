e46763a5339fe68905ddd56c06d92240b6758bd3
Quahu <quahuu@gmail.com>
2020-05-09 20:27:12 +0200

    Made BandCollection implement IList<float>.

--- Pahoe/BandCollection.cs
+++ Pahoe/BandCollection.cs
@@ -5,7 +5,7 @@ using System.Threading.Tasks;
 
 namespace Pahoe
 {
-    public sealed class BandCollection : IReadOnlyList<float>
+    public sealed class BandCollection : IList<float>, IReadOnlyList<float>
     {
         public int Count => 15;
 
@@ -31,6 +31,8 @@ namespace Pahoe
             }
         }
 
+        bool ICollection<float>.IsReadOnly => true;
+
         private readonly LavalinkPlayer _player;
         internal readonly float[] Bands;
         internal readonly float[] PreviousBands; // Used for not sending unnecessary JSON values.
@@ -54,6 +56,18 @@ namespace Pahoe
         public ValueTask UpdateBandsAsync()
             => _player.UpdateBandsAsync();
 
+        public int IndexOf(float item)
+            => Array.IndexOf(Bands, item);
+
+        public void Clear()
+            => Array.Clear(Bands, 0, 15);
+
+        public bool Contains(float item)
+            => Array.IndexOf(Bands, item) != -1;
+
+        public void CopyTo(float[] array, int arrayIndex)
+            => Bands.CopyTo(array, arrayIndex);
+
         public IEnumerator<float> GetEnumerator()
         {
             for (var i = 0; i < 15; i++)
@@ -62,5 +76,17 @@ namespace Pahoe
 
         IEnumerator IEnumerable.GetEnumerator()
             => GetEnumerator();
+
+        void IList<float>.Insert(int index, float item)
+            => throw new NotSupportedException();
+
+        void IList<float>.RemoveAt(int index)
+            => throw new NotSupportedException();
+
+        void ICollection<float>.Add(float item)
+            => throw new NotSupportedException();
+
+        bool ICollection<float>.Remove(float item)
+            => throw new NotSupportedException();
     }
 }
